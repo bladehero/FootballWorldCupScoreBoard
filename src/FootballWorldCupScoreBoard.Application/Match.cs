db62681a2ddfdafbd34b847c2daaad65068819da
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 16:14:47 +0200

    feat: add create method that wraps scores

---
+++ src/FootballWorldCupScoreBoard.Application/Match.cs
@@ -0,0 +1,19 @@
+﻿namespace FootballWorldCupScoreBoard.Application;
+
+public class Match
+{
+    private TeamScore _homeTeamScore = null!;
+    private TeamScore _awayTeamScore = null!;
+
+    public Team HomeTeam => _homeTeamScore.Team;
+    public Team AwayTeam => _awayTeamScore.Team;
+
+    private Match() { }
+
+    public static Match Create(Team homeTeam, Team awayTeam) =>
+        new()
+        {
+            _homeTeamScore = TeamScore.CreateFor(homeTeam),
+            _awayTeamScore = TeamScore.CreateFor(awayTeam)
+        };
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.Tests/MatchTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace FootballWorldCupScoreBoard.Application.Tests;
+
+public class MatchTests
+{
+    private static readonly Team HomeTeam = Team.Create("Spain");
+    private static readonly Team AwayTeam = Team.Create("Italy");
+
+    [Fact]
+    public void Create_Always_ShouldReturnMatchWithTwoProvidedTeams()
+    {
+        // Act
+        var actual = Match.Create(HomeTeam, AwayTeam);
+
+        // Assert
+        actual
+            .Should()
+            .NotBeNull()
+            .And
+            .BeOfType<Match>()
+            .And
+            .BeEquivalentTo(new { HomeTeam, AwayTeam });
+    }
+}
