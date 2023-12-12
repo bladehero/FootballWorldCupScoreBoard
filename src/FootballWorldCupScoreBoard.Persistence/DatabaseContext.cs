b5b3417bdcec441212bec8a0bf937b6fa71d0b06
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 18:16:55 +0200

    feat: add database context with game collection

---
+++ src/FootballWorldCupScoreBoard.Persistence/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using FootballWorldCupScoreBoard.Persistence.Games;
+
+namespace FootballWorldCupScoreBoard.Persistence;
+
+public class DatabaseContext
+{
+    public GameCollection Games { get; set; }
+
+    public DatabaseContext()
+    {
+        Games = new GameCollection();
+    }
+}
