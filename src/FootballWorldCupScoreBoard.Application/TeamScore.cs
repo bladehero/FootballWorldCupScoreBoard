f99e74bc294665fb29c60f1d8a578ae292eba808
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 16:08:14 +0200

    Create Team Score Entity (#4)
    The `TeamScore` entity wraps specific `Team`sand `Score` together.
    This approach allows for independently increasing each score and making
    next `Match` entity

---
+++ src/FootballWorldCupScoreBoard.Application/TeamScore.cs
@@ -0,0 +1,19 @@
+﻿namespace FootballWorldCupScoreBoard.Application;
+
+public record TeamScore
+{
+    public Team Team { get; init; } = null!;
+    public byte Score { get; private set; }
+
+    private TeamScore() { }
+
+    public static TeamScore CreateFor(Team team) => new() { Team = team, Score = 0 };
+
+    public void Increase()
+    {
+        checked
+        {
+            Score++;
+        }
+    }
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.Tests/TeamScoreTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+
+namespace FootballWorldCupScoreBoard.Application.Tests;
+
+public class TeamScoreTests
+{
+    private const int MoreThanByteCanStore = byte.MaxValue + 1;
+    private const string AnyName = "Italy";
+    private static readonly Team AnyTeam = Team.Create(AnyName);
+
+    [Fact]
+    public void CreateFor_Always_ShouldReturnTeamScore()
+    {
+        // Act
+        var actual = CreateSut();
+
+        // Assert
+        actual
+            .Should()
+            .NotBeNull()
+            .And
+            .BeOfType<TeamScore>()
+            .And
+            .BeEquivalentTo(new { Team = new { Name = AnyName }, Score = 0 });
+    }
+
+    [Fact]
+    public void Increase_WhenInvokedOneTime_ShouldSetScoreToOne()
+    {
+        // Arrange
+        var sut = CreateSut();
+
+        // Act
+        sut.Increase();
+
+        // Assert
+        sut.Score.Should().Be(1);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(5)]
+    [InlineData(255)]
+    public void Increase_WhenInvokedMoreThanOnce_ShouldSetScoreToAmountOfInvocationTimes(byte times)
+    {
+        // Arrange
+        var sut = CreateSut();
+
+        // Act
+        RepeatIncreaseFor(sut, times);
+
+        // Assert
+        sut.Score.Should().Be(times);
+    }
+
+    [Fact]
+    public void Increase_WhenScoreMoreThan255_ShouldThrowOverflowException()
+    {
+        // Arrange
+        var sut = CreateSut();
+
+        // Act
+        var action = () => RepeatIncreaseFor(sut, MoreThanByteCanStore);
+
+        // Assert
+        action.Should().ThrowExactly<OverflowException>();
+    }
+
+    private static TeamScore CreateSut() => TeamScore.CreateFor(AnyTeam);
+
+    private static void RepeatIncreaseFor(TeamScore teamScore, int times)
+    {
+        for (var _ = 0; _ < times; _++)
+        {
+            teamScore.Increase();
+        }
+    }
+}
