557a25613ab845f724123ccf6ba6a42822ad830a
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 18:38:36 +0200

    feat: add start new method

---
+++ src/FootballWorldCupScoreBoard.Application/Scoring/ScoreBoard.cs
@@ -0,0 +1,20 @@
+﻿using FootballWorldCupScoreBoard.Application.Matches;
+
+namespace FootballWorldCupScoreBoard.Application.Scoring;
+
+public class ScoreBoard
+{
+    private readonly IMatchProvider _matchProvider;
+    private IMatch? _current;
+
+    public ScoreBoard(IMatchProvider matchProvider)
+    {
+        _matchProvider = matchProvider;
+    }
+
+    public IMatch StartNew(string homeTeamName, string awayTeamName)
+    {
+        _current = _matchProvider.Create(homeTeamName, awayTeamName);
+        return _current;
+    }
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.UnitTests/Scoring/ScoreBoardTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using FootballWorldCupScoreBoard.Application.Matches;
+using FootballWorldCupScoreBoard.Application.Scoring;
+using Moq;
+
+namespace FootballWorldCupScoreBoard.Application.UnitTests.Scoring;
+
+public class ScoreBoardTests
+{
+    private const string HomeTeamName = "Spain";
+    private const string AwayTeamName = "Italy";
+    private readonly Mock<IMatchProvider> _matchProviderMock = new();
+    private readonly ScoreBoard _sut;
+
+    public ScoreBoardTests()
+    {
+        _sut = new ScoreBoard(_matchProviderMock.Object);
+    }
+
+    [Fact]
+    public void Start_Always_ProduceNewMatch()
+    {
+        // Arrange
+        var expected = new Mock<IMatch>().Object;
+        _matchProviderMock.Setup(x => x.Create(HomeTeamName, AwayTeamName)).Returns(expected);
+
+        // Act
+        var actual = _sut.StartNew(HomeTeamName, AwayTeamName);
+
+        // Assert
+        using (new AssertionScope())
+        {
+            actual.Should().Be(expected);
+            _matchProviderMock.VerifyAll();
+        }
+    }
+}
