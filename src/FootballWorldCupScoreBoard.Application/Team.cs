e12eeecdeef3a1741b251a289f9f3ca458d6d726
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 14:43:43 +0200

    add entity

---
+++ src/FootballWorldCupScoreBoard.Application/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballWorldCupScoreBoard.Application;
+
+public record Team
+{
+    public string Name { get; init; }
+
+    private Team() { }
+
+    public static Team Create() => new();
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.Tests/TeamTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace FootballWorldCupScoreBoard.Application.Tests;
+
+public class TeamTests
+{
+    [Fact]
+    public void Create_Always_ShouldReturnTeam()
+    {
+        // Act
+        var actual = Team.Create();
+
+        // Assert
+        actual.Should().NotBeNull().And.BeOfType<Team>();
+    }
+}
