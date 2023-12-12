587ccd8c9b95ae1f25fc8744f5dc0b7c6fe05a49
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 15:39:05 +0200

    feat: create team score entity

---
+++ src/FootballWorldCupScoreBoard.Application/TeamScore.cs
@@ -0,0 +1,11 @@
+﻿namespace FootballWorldCupScoreBoard.Application;
+
+public record TeamScore
+{
+    public Team Team { get; init; } = null!;
+    public byte Score { get; init; }
+
+    private TeamScore() { }
+
+    public static TeamScore CreateFor(Team team) => new() { Team = team, Score = 0 };
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.Tests/TeamScoreTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace FootballWorldCupScoreBoard.Application.Tests;
+
+public class TeamScoreTests
+{
+    private const string AnyName = "Italy";
+    private static readonly Team AnyTeam = Team.Create(AnyName);
+
+    [Fact]
+    public void CreateFor_Always_ShouldReturnTeamScore()
+    {
+        // Act
+        var actual = TeamScore.CreateFor(AnyTeam);
+
+        // Assert
+        actual
+            .Should()
+            .NotBeNull()
+            .And
+            .BeOfType<TeamScore>()
+            .And
+            .BeEquivalentTo(new { Team = new { Name = AnyName }, Score = 0 });
+    }
+}
