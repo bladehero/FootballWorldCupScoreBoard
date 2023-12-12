6b4a7ebb57682310c8cfaa7ae904bc5c712a8af9
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 18:22:59 +0200

    feat: add dependency injection for persistence layer

---
+++ src/FootballWorldCupScoreBoard.Persistence/DependencyInjectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FootballWorldCupScoreBoard.Persistence;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services) =>
+        services.AddSingleton<DatabaseContext>();
+}
---
+++ tests/FootballWorldCupScoreBoard.Persistence.UnitTests/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FootballWorldCupScoreBoard.Persistence.UnitTests;
+
+public class DependencyInjectionExtensionsTests
+{
+    [Fact]
+    public void AddPersistence_Always_AddSingletonOfDatabaseContext()
+    {
+        // Arrange
+        var sut = new ServiceCollection();
+
+        // Act
+        sut.AddPersistence();
+
+        // Assert
+        sut.Should()
+            .ContainSingle(
+                x =>
+                    x.Lifetime == ServiceLifetime.Singleton
+                    && x.ImplementationType == typeof(DatabaseContext)
+            );
+    }
+}
