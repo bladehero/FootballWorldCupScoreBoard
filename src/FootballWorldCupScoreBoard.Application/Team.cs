69c18cda7a5e7805b47d26a0e100f4b6f6f08a67
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-12 15:01:51 +0200

    Create Team Entity (#3)
    An atomic piece of scoreboard is a team.
    Here is a record with the factory method to produce a team with a
    fulfilled name.

---
+++ src/FootballWorldCupScoreBoard.Application/Team.cs
@@ -0,0 +1,16 @@
+﻿namespace FootballWorldCupScoreBoard.Application;
+
+public record Team
+{
+    public string Name { get; init; } = null!;
+
+    private Team() { }
+
+    public static Team Create(string name)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        return new Team { Name = name };
+    }
+
+    public override string ToString() => Name;
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.Tests/TeamTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace FootballWorldCupScoreBoard.Application.Tests;
+
+public class TeamTests
+{
+    private const string AnyName = "Italy";
+
+    [Fact]
+    public void Create_WhenValidName_ShouldReturnTeam()
+    {
+        // Act
+        var actual = Team.Create(AnyName);
+
+        // Assert
+        using (new AssertionScope())
+        {
+            actual.Should().NotBeNull().And.BeOfType<Team>();
+            actual.Name.Should().Be(AnyName);
+        }
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Create_WhenNameIsNullOrEmpty_ShouldThrowArgumentException(string? name)
+    {
+        // Act
+        var action = () => Team.Create(name!);
+
+        // Assert
+        action.Should().Throw<ArgumentException>().Which.ParamName.Should().Be("name");
+    }
+
+    [Fact]
+    public void ToString_Always_ShouldReturnTeamName()
+    {
+        // Arrange
+        var sut = Team.Create(AnyName);
+
+        // Act
+        var actual = sut.ToString();
+
+        // Assert
+        actual.Should().Be(AnyName);
+    }
+}
