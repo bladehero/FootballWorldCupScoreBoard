9fc7c6e7d175c71e35532fb4cc24b78b88e60b62
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-13 11:09:17 +0200

    feat: add dependency injection for application layer

---
+++ src/FootballWorldCupScoreBoard.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using FootballWorldCupScoreBoard.Application.Matches;
+using FootballWorldCupScoreBoard.Application.Scoring;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FootballWorldCupScoreBoard.Application;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) =>
+        services.AddTransient<IMatchProvider, MatchProvider>().AddScoped<IScoreBoard, ScoreBoard>();
+}
---
+++ tests/FootballWorldCupScoreBoard.Application.UnitTests/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using FootballWorldCupScoreBoard.Application.Matches;
+using FootballWorldCupScoreBoard.Application.Scoring;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FootballWorldCupScoreBoard.Application.UnitTests;
+
+public class DependencyInjectionExtensionsTests
+{
+    [Fact]
+    public void AddApplication_Always_AddSingletonOfDatabaseContext()
+    {
+        // Arrange
+        var sut = new ServiceCollection();
+
+        // Act
+        sut.AddApplication();
+
+        // Assert
+        using (new AssertionScope())
+        {
+            VerifyServiceInjection<IMatchProvider, MatchProvider>(sut, ServiceLifetime.Transient);
+            VerifyServiceInjection<IScoreBoard, ScoreBoard>(sut, ServiceLifetime.Scoped);
+        }
+    }
+
+    private static void VerifyServiceInjection<TService, TImplementation>(
+        ServiceCollection sut,
+        ServiceLifetime lifetime
+    )
+    {
+        sut.Should()
+            .Contain(
+                x =>
+                    x.Lifetime == lifetime
+                    && x.ServiceType == typeof(TService)
+                    && x.ImplementationType == typeof(TImplementation)
+            );
+    }
+}
