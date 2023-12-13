e52aaa42fa27ce5518a074e09ae08e34385f2a95
Nick Dermenzhi <nikita.dermenzhi@gmail.com>
2023-12-13 09:52:43 +0200

    feat: extract match score details from behavior

--- src/FootballWorldCupScoreBoard.Application/Matches/IMatch.cs
+++ src/FootballWorldCupScoreBoard.Application/Matches/IMatch.cs
@@ -1,11 +1,7 @@
 ﻿namespace FootballWorldCupScoreBoard.Application.Matches;
 
-public interface IMatch
+public interface IMatch : IMatchScore
 {
-    Team HomeTeam { get; }
-    byte HomeTeamScore { get; }
-    Team AwayTeam { get; }
-    byte AwayTeamScore { get; }
     void HomeTeamScoresGoal();
     void AwayTeamScoresGoal();
 }
